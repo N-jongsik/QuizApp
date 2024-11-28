a38b1e494477182a6f684db3a4a9c1681d83a243
minwook <minukhan>
2024-11-28 11:22:32 +0900

    Timer 5

--- app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -16,7 +16,7 @@ class QuizActivity : AppCompatActivity() {
     private lateinit var option1: FrameLayout
     private lateinit var option2: FrameLayout
 
-    private var timeLeft: Int = 10 // 타이머 시간 (초)
+    private var timeLeft: Int = 5 // 타이머 시간 (초)
     private var progress: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +45,7 @@ class QuizActivity : AppCompatActivity() {
             override fun onTick(millisUntilFinished: Long) {
                 timeLeft--
                 timerTextView.text = "남은 시간 : ${timeLeft}초"
-                progress += 10
+                progress += 20
                 progressBar.progress = progress
             }
 
