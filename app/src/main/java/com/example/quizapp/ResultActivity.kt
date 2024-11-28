426a6f247007c05dd672deadca6532aef9285cb9
N-jongsik <jongsik0625@naver.com>
2024-11-28 11:57:51 +0900

    navbar

--- app/src/main/java/com/example/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -1,6 +1,8 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,22 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonRetry = findViewById<Button>(R.id.buttonRetry)
+
+        buttonRetry.setOnClickListener {
+            navigateToQuizScreen()
         }
+    }
+    private fun navigateToMainScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish() // 현재 액티비티 종료
+    }
+
+    private fun navigateToQuizScreen() {
+        val intent = Intent(this, QuizActivity::class.java)
+        startActivity(intent)
+        finish() // 현재 액티비티 종료
     }
 }
